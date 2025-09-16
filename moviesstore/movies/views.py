9cce7eee0612641f080a06896e2c73776603cd8e
Rick <ricklee2487@gmail.com>
2025-09-16 14:28:00 -0400

    Report review feature added

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def show(request, id):
     movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    reviews = Review.objects.filter(movie=movie, reported=False)
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
@@ -56,4 +56,11 @@ def edit_review(request, id, review_id):
 def delete_review(request, id, review_id):
     review = get_object_or_404(Review, id=review_id, user=request.user)
     review.delete()
+    return redirect('movies.show', id=id)
+
+@login_required
+def report_review(request, id, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    review.reported = True
+    review.save()
     return redirect('movies.show', id=id)
